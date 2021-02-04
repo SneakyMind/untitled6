0dbbcedc4deab9bbb0a3259db46b9a58eaef8960
Professional <hate4fake@gmail.com>
2021-02-04 20:17:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import Todo from "./components/Todo";
 import './App.css';
+import React, { useState } from 'react';
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(props) {
+    const [tasks, setTasks] = useState(props.tasks)
+    const taskList = tasks.map(task => (
+            <Todo
+                id={task.id}
+                name={task.name}
+                completed={task.completed}
+                key={task.id}
+            />
+        )
+    );
+    return (
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <Form />
+            <div className="filters btn-group stack-exception">
+                <FilterButton name="All" />
+                <FilterButton name="Active" />
+                <FilterButton name="Completed" />
+            </div>
+            <h2 id="list-heading">
+                4 tasks remaining
+            </h2>
+            <ul
+                role="list"
+                className="todo-list stack-large stack-exception"
+                aria-labelledby="list-heading"
+            >
+                {taskList}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
